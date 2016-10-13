a55b2348716d70224066c62df30809dc63324a91
Abraao Mota <abraaomot@outlook.com>
2016-10-13 21:06:25 +0100

    1.2 - String permutation

--- ch1_check_permutation/src/CheckPermutation.java
+++ ch1_check_permutation/src/CheckPermutation.java
@@ -1,7 +1,50 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Abraao Mota on 13/10/2016.
  */
+/*
+ * Question 1.2 - Check permutation - Page 90 - Hints 1, 84, 122, 131
+ *
+ * Given two strings, write a method to decide if one is a permutation of the other.
+ *
+ */
+
+/*
+    Intuition: Try and hash each character as in 1.1. Count each character seen in an array to symbolise count of chars
+    in first string.
+    When going through the second string, generate character hash and check array at that place. If the count is greater
+    than 0, decrease count and move onto next element. If 0, stop and return false.
+    Assume alphabet only for ease of hash function.
+
+    Complexity:
+
+    if s1 length = n, s2 length = m,
+    O(m + n)
+    However, solution uses up space.
+ */
+
 public class CheckPermutation {
+    public boolean isPermutation(String s2, String s1) {
+        int[] seenChars = new int[26];
+
+        if (s2.length() != s1.length()) return false;
+
+        for (char c : s1.toCharArray()) {
+            int arrayPosition = hashChar(c);
+            seenChars[arrayPosition]++;
+        }
+
+        for (char c : s2.toCharArray()) {
+            int arrayPosition = hashChar(c);
+            if (seenChars[arrayPosition] == 0) {
+                return false;
+            } else {
+                seenChars[arrayPosition] -= 1;
+            }
+        }
+        return true;
+    }
+
+    private int hashChar(char c) {
+        return (int) c - (int) 'a';
+    }
 }
--- ch1_check_permutation/src/CheckPermutationTest.java
+++ ch1_check_permutation/src/CheckPermutationTest.java
@@ -1,7 +1,51 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Abraao Mota on 13/10/2016.
  */
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CheckPermutationTest {
+
+    private static final CheckPermutation checker = new CheckPermutation();
+
+    @Test
+    public void smallStringPermutation() {
+        String s1 = "abc";
+        String s2 = "cba";
+        boolean result = checker.isPermutation(s2, s1);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void noPermutation() {
+        String s1 = "abcdefg";
+        String s2 = "bloop";
+        boolean result = checker.isPermutation(s2, s1);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void reverseAlphabet() {
+        String s1 = "abcdefghijklmnopqrstuvwxyz";
+        String s2 = "zyxwvutsrqponmlkjihgfedcba";
+        boolean result = checker.isPermutation(s2, s1);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void emptyStrings() {
+        String s1 = "";
+        String s2 = "";
+        boolean result = checker.isPermutation(s2, s1);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void notQuiteEqual() {
+        String s1 = "aaaaa";
+        String s2 = "aaaa";
+        boolean result = checker.isPermutation(s2, s1);
+        assertEquals(false, result);
+    }
 }
